37c4d5f9ae3709334b147ded03d398e9a0750c75
Zhou Jintian <18307100015@fudan.edu.cn>
2020-08-31 22:06:31 +0800

    Revert "Revert "update config""
    This reverts commit 130db0bb744599b451cfdb5d2d44e392877a50b5.

--- config.py
+++ config.py
@@ -1,8 +1,9 @@
 from multiprocessing import cpu_count
 
 # This is where you put the data
-DATA_PATH = r'D:\Dataset\ThinkTank'
+#DATA_PATH = r'D:\Dataset\ThinkTank'
 # DATA_PATH = r'C:\Users\impor\PycharmProjects\ThinkTank\data'
+DATA_PATH = r'C:\Users\Jin tian Zhou\PycharmProjects\thinktank'
 USE_DATA = 'asn'  # 'asn' or 'dblp', 'asn' for academic social network
 
 # Count for cpu cores, used for multi processing
